ea6701ff2ab52056af8b9a94f1e25716d88e029e
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2024-03-07 08:01:57 +0700

    Tambah Layout GUI

--- testmod3/Form1.cs
+++ testmod3/Form1.cs
@@ -16,5 +16,40 @@ namespace testmod3
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
